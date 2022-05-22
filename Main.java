195eb70afd5f548ff6e1f8f17405904ea10fc857
Vivi <shoter998@gmail.com>
2022-05-22 19:32:30 +0200

    Revert "test"
    This reverts commit 789b51e6efdd1ad9ca4d55bb4d51fd72867842ae.

--- Main.java
+++ Main.java
@@ -1,34 +1,5 @@
 class Main {
-
-    public static int[][] BOARD = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
-
     public static void main(String[] args) {
-        drawBoard(BOARD);
-    }
 
-    public static void drawBoard(int[][] board) {
-        System.out.println(" ---  ---  ---");
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                char symbol;
-                switch (board[i][j]) {
-                    case 0:
-                        symbol = ' ';
-                        break;
-                    case 1:
-                        symbol = 'X';
-                        break;
-                    case 2:
-                        symbol = 'O';
-                        break;
-                    default:
-                        symbol = ' ';
-                        break;
-                }
-                System.out.printf("| %s |", symbol);
-            }
-            System.out.println("");
-            System.out.println(" ---  ---  ---");
-        }
     }
 }
